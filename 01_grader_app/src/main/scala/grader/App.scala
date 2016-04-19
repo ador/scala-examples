e506a43864ef469d2c60a090c48d45a5b0c88de4
Matthias Reik <matthias.reik@meltwater.com>
2016-04-19 09:40:20 +0200

    adding some tests

--- 01_grader_app/src/main/scala/grader/App.scala
+++ 01_grader_app/src/main/scala/grader/App.scala
@@ -15,12 +15,12 @@ object GraderApp extends App {
   }
 
   def grade(score: Int):Grade = score match {
-      case a if (0<=a && a < 40) => F
+      case a if (a < 40) => F
       case a if (40<=a && a < 50) => E
       case a if (50<=a && a < 60) => D
       case a if (60<=a && a < 70) => C
       case a if (70<=a && a < 80) => B
-      case _ => A    
+      case _ => A
    }
 }
 
--- 01_grader_app/src/test/scala/grader/GraderTest.scala
+++ 01_grader_app/src/test/scala/grader/GraderTest.scala
@@ -1,12 +1,38 @@
 package grader
 
 import org.scalatest.FlatSpec
+import GraderApp.Grade._
 
 class GraderTest extends FlatSpec {
 
-  "Grader" should "grade a zero valued test to 1" in {
-    val testScore1 = 0
-    val grade = GraderApp.grade(0)
-    assert(1 == grade)
+  "Grader" should "grade a zero valued test to F" in {
+    assert(F == GraderApp.grade(0))
+  }
+  "Grader" should "grade a negative valued test to F" in {
+    assert(F == GraderApp.grade(-10))
+  }
+  "Grader" should "grade a 41 valued test to E" in {
+    assert(E == GraderApp.grade(41))
+  }
+  "Grader" should "grade a 51 valued test to D" in {
+    assert(D == GraderApp.grade(51))
+  }
+  "Grader" should "grade a 61 valued test to C" in {
+    assert(C == GraderApp.grade(61))
+  }
+  "Grader" should "grade a 71 valued test to B" in {
+    assert(B == GraderApp.grade(71))
+  }
+  "Grader" should "grade a 80 valued test to A" in {
+    assert(A == GraderApp.grade(81))
+  }
+  "Grader" should "grade a 90 valued test to A" in {
+    assert(A == GraderApp.grade(90))
+  }
+  "Grader" should "grade a 100 valued test to A" in {
+    assert(A == GraderApp.grade(100))
+  }
+  "Grader" should "grade a 101 valued test to A" in {
+    assert(A == GraderApp.grade(101))
   }
 }
