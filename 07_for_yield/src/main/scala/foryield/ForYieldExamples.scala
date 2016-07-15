8f0f7a76a8d84741a4b72e386ab0b59504b96bc2
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-07-15 18:29:53 +0200

    for comprehension basic examples

---
+++ 07_for_yield/src/main/scala/foryield/ForYieldExamples.scala
@@ -0,0 +1,45 @@
+package foryield
+
+object ForYieldExamples extends App {
+
+  case class User(name: String)
+  case class TweetMsg(id: Long, user: User, msg: String)
+
+  def createTweetList: List[TweetMsg] = {
+    val user1 = new User("ador")
+    val user2 = new User("szeli")
+    val t1 = new TweetMsg(9243012L, user1, "scala program")
+    val t2 = new TweetMsg(9534594L, user2, "ice cream")
+    val t3 = new TweetMsg(9811235L, user1, "playing with scala")
+    val tweetList: List[TweetMsg] = List(t1, t2 ,t3)
+    tweetList
+  }
+
+  def getMessagesOnly(tweets: List[TweetMsg]): List[String] = {
+    val messageList = for (tw <- tweets) yield tw.msg
+    messageList
+  }
+  
+  def getMessagesWithScalaOnly(tweets: List[TweetMsg]): List[String] = {
+    val filteredMessageList = for {
+      tw <- tweets
+      if tw.msg.contains("scala")
+    } yield tw.msg
+    filteredMessageList
+  }
+
+  def getAllUserMsgPairs(tweets: List[TweetMsg]): List[(User, String)] = {
+    for {
+      tw <- tweets
+      user <- tweets.map(_.user)
+    } yield (user, tw.msg)
+  }
+  
+  override def main(args: Array[String]): Unit = {
+    println("Hello to for comprehensions in Scala!")
+    val tweets = createTweetList
+    for (tw <- tweets) {
+      println("Tweeter was: " + tw.user + " and message was: " + tw.msg)
+    }
+  }
+}
---
+++ 07_for_yield/src/test/scala/foryield/ForYieldExamplesTest.scala
@@ -0,0 +1,33 @@
+package foryield
+
+import org.scalatest.FlatSpec
+import ForYieldExamples._
+
+class ForYieldExamplesTest extends FlatSpec {
+
+  "ForYieldExample" should "return a list of String tweet messages" in {
+    val inputTweets = ForYieldExamples.createTweetList
+    val messages = ForYieldExamples.getMessagesOnly(inputTweets)
+    assert(3 == messages.length)
+    assert("scala program" == messages(0))
+  }
+
+  "it" should "return only some messages " in {
+    val inputTweets = ForYieldExamples.createTweetList
+    val messages = ForYieldExamples.getMessagesWithScalaOnly(inputTweets)
+    assert(2 == messages.length)
+    for (m <- messages)
+      assert(m.contains("scala"))
+  }
+
+  "it" should "return all possible user-message pairs " in {
+    val inputTweets = ForYieldExamples.createTweetList
+    val allPairs = ForYieldExamples.getAllUserMsgPairs(inputTweets)
+    assert(9 == allPairs.length)
+    assert(allPairs(0)._1.name == "ador")
+    assert(allPairs(0)._2 == "scala program")
+    assert(allPairs(8)._1.name == "ador")
+    assert(allPairs(8)._2 == "playing with scala")
+  }
+
+}
