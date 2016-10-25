f4f5953f845c156464e9e5bc1383d3203ed94892
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-10-25 14:42:56 +0200

    testing all

--- scripts/validator.sh
+++ scripts/validator.sh
@@ -1,8 +1,35 @@
 #!/bin/bash -eux
 
-echo "hello "
 pushd 01_grader_app
 	sbt test
 popd
+
+pushd 02_pattern_match_app
+	sbt test
+popd
+
+pushd 03_lists
+	sbt test
+popd
+
+pushd 04_tuples
+	sbt test
+popd
+
+pushd 05_maps
+	sbt test
+popd
+
+pushd 06_options
+	sbt test
+popd
+
+pushd 07_for_yield
+	sbt test
+popd
+
+#pushd 08_futures
+#	sbt test
+#popd
 
 exit 0
